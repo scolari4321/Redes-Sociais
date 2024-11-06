a0914eea1c9027a97ff7f91adae2578d1fed6d50
scolari4321 <127404238+scolari4321@users.noreply.github.com>
2024-11-06 16:15:31 -0300

    Add files via upload

--- info.js
+++ info.js
@@ -10,7 +10,7 @@ async function vizualizarInformacoesGlobais() {
     const porcentagemConectada = ((pessoasConectadas / pessoasNoMundo ) * 100).toFixed(2)
 
     const paragrafo = document.createElement('p')
-    paragrafo.classList.add('graficos-container__texto')
+    paragrafo.classList.add('graficos-container_texto')
     paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo} bilhões</span> de pessoas e que aproximadamente <span>${pessoasConectadas} bilhões</span> estão conectadas em alguma rede social e passam em média <span>${horas} horas</span> e <span>${minutos} minutos</span> conectadas.<br>Isso significa que aproximadamente <span>${porcentagemConectada}%</span> de pessoas estão conectadas em alguma rede social.`
 
     const container = document.getElementById('graficos-container')
